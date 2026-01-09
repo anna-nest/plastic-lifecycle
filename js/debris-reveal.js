5232107097d35155b01dcb7e2efe320e29e0689c
anna-nest <annanest.design@gmail.com>
2026-01-09 11:29:34 +0000

    Add files via upload

--- js/debris-reveal.js
+++ js/debris-reveal.js
@@ -14,7 +14,7 @@ class DebrisReveal {
     this.fadeProgress = 1;
     
     this.config = {
-      rotationSpeed: 0.09,
+      rotationSpeed: 0.08,
       initialSize: 2, // 5% of viewport
       initialOpacity: 0.1, // starts at 20%
       maxOpacity: 1, // grows to 100%
